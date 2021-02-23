870d48a9d7fb2817de8dc2df62759de0f946f68f
monaqa <mogassy@yahoo.co.jp>
2021-02-23 10:52:42 +0900

    avoid telescope error

--- .config/nvim/scripts/plugin.lua
+++ .config/nvim/scripts/plugin.lua
@@ -50,17 +50,6 @@ require('telescope').setup{
         '--files',
     },
     mappings = {
-      i = {
-        -- To disable a keymap, put [map] = false
-        -- So, to not map "<C-n>", just put
-        -- ["<C-x>"] = false,
-        -- Otherwise, just set the mapping to the function that you want it to be.
-        ["<C-i>"] = actions.goto_file_selection_split,
-        -- Add up multiple actions
-        ["<CR>"] = actions.goto_file_selection_edit + actions.center,
-        -- You can perform as many actions in a row as you like
-        -- ["<CR>"] = actions.goto_file_selection_edit + actions.center + my_cool_custom_action,
-      },
       n = {
         ["<Esc>"] = actions.close,
       },
