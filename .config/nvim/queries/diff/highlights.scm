aafa3d5c48d6456b6ece3104d8f26e8d72737a54
monaqa <cmonaqa@gmail.com>
2022-11-30 09:37:03 +0900

    add diff highlight config

---
+++ .config/nvim/queries/diff/highlights.scm
@@ -0,0 +1,9 @@
+(addition ("+") @text.diff.addsign) @text.diff.add
+(deletion ("-") @text.diff.delsign) @text.diff.delete
+
+(new_file) @text.diff.add
+(old_file) @text.diff.delete
+
+(commit) @constant
+(location) @attribute
+(command) @function
