e8db2d61ade771c2eb60f8c183c34703fe6327c1
monaqa <cmonaqa@gmail.com>
2023-09-06 17:24:14 +0900

    feat: nvim-comment -> Comment.nvim

--- .config/nvim/lua/rc/plugins.lua
+++ .config/nvim/lua/rc/plugins.lua
@@ -629,16 +629,54 @@ add {
         vim.g["deepl#auth_key"] = vim.fn.getenv "DEEPL_API_KEY"
     end,
 }
+
 add {
-    "terrortylor/nvim-comment",
-    name = "nvim_comment",
-    keys = { { ",", mode = { "n", "x" } } },
+    "numToStr/Comment.nvim",
+    keys = {
+        { ",", mode = { "n", "v" } },
+        { ",,", mode = { "n" } },
+    },
     opts = {
-        line_mapping = ",,",
-        operator_mapping = ",",
-        -- comment_chunk_text_object = "im",
+        ---Add a space b/w comment and the line
+        padding = true,
+        -- ---Whether the cursor should stay at its position
+        -- sticky = true,
+        -- ---Lines to be ignored while (un)comment
+        -- ignore = nil,
+        ---LHS of toggle mappings in NORMAL mode
+        toggler = {
+            ---Line-comment toggle keymap
+            line = ",,",
+        },
+        ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+        opleader = {
+            ---Line-comment keymap
+            line = ",",
+        },
+        -- ---LHS of extra mappings
+        -- extra = {
+        --     ---Add comment on the line above
+        --     above = "gcO",
+        --     ---Add comment on the line below
+        --     below = "gco",
+        --     ---Add comment at the end of line
+        --     eol = "gcA",
+        -- },
+        -- ---Enable keybindings
+        -- ---NOTE: If given `false` then the plugin won't create any mappings
+        -- mappings = {
+        --     ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+        --     basic = true,
+        --     ---Extra mapping; `gco`, `gcO`, `gcA`
+        --     extra = true,
+        -- },
+        -- ---Function to call before (un)comment
+        -- pre_hook = nil,
+        -- ---Function to call after (un)comment
+        -- post_hook = nil,
     },
 }
+
 add { "thinca/vim-qfreplace", ft = { "qf" } }
 -- add { "thinca/vim-quickrun" }
 add { "tyru/capture.vim", cmd = { "Capture" } }
