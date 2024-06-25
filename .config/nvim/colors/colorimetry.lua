79d8e22fdb17c5edbd9b0b04d44bbd0002697028
monaqa <cmonaqa@gmail.com>
2024-06-25 17:01:32 +0900

    update color config

--- .config/nvim/colors/colorimetry.lua
+++ .config/nvim/colors/colorimetry.lua
@@ -1,5 +1,29 @@
 -- vim:fdm=marker:fmr=§§,■■
 
+-- §§1 types
+---@class HlSpec
+---@field fg? string color name or "#RRGGBB", see note.
+---@field bg? string color name or "#RRGGBB", see note.
+---@field sp? string color name or "#RRGGBB"
+---@field blend? integer between 0 and 100
+---@field bold? boolean
+---@field standout? boolean
+---@field underline? boolean
+---@field undercurl? boolean
+---@field underdouble? boolean
+---@field underdotted? boolean
+---@field underdashed? boolean
+---@field strikethrough? boolean
+---@field italic? boolean
+---@field reverse? boolean
+---@field nocombine? boolean
+---@field link? string name of another highlight group to link to, see |:hi-link|.
+---@field default? boolean Don't override existing definition |:hi-default|
+---@field ctermfg? string Sets foreground of cterm color |ctermfg|
+---@field ctermbg? string Sets background of cterm color |ctermbg|
+---@field cterm? string cterm attribute map, like |highlight-args|. If not set, cterm attributes will match those from the attribute map documented above.
+---@field force? boolean if true force update the highlight group when it exists.
+
 -- §§1 設定
 -- if vim.g.colors_name then
 --     vim.cmd("hi clear")
@@ -125,7 +149,7 @@ vim.g.terminal_color_14 = fg.e1
 vim.g.terminal_color_15 = fg.w1
 
 -- §§1 defaults
-sethl("Normal") { fg = fg.w0, bg = bg.w1 }
+sethl("Normal") { fg = fg.w1, bg = bg.w1 }
 
 sethl("ColorColumn") { bg = bg.w2 }
 sethl("Conceal") { fg = fg.w5 }
@@ -142,7 +166,7 @@ sethl("EndOfBuffer") { bg = bg.w2 }
 sethl("TermCursor") { reverse = true }
 sethl("ErrorMsg") { fg = fg.r4 }
 sethl("WinSeparator") { fg = fg.w4 }
-sethl("Folded") { bg = bg.p0 }
+sethl("Folded") { fg = fg.v1 }
 sethl("FoldColumn") { bg = bg.w1, fg = fg.w4 }
 sethl("SignColumn") { bg = bg.w1, fg = fg.w4 }
 sethl("IncSearch") { bg = bg.b2 }
@@ -164,13 +188,17 @@ sethl("Question") { fg = fg.o2 }
 sethl("QuickfixLine") { fg = fg.e3 }
 sethl("Search") { bg = bg.b4 }
 sethl("SpecialKey") { fg = fg.w5, bg = bg.w4 }
+sethl("SpellBad") { underdotted = true, fg = fg.r0 }
+sethl("SpellCap") { underdotted = true, fg = fg.w0 }
+sethl("SpellLocal") { underdotted = true, fg = fg.w0 }
+sethl("SpellRare") { underdotted = true, fg = fg.w0 }
 sethl("StatusLine") { reverse = true }
-sethl("Visual") { bg = bg.p2 }
+sethl("Visual") { bg = bg.p4 }
 sethl("WarningMsg") { fg = fg.y1 }
-sethl("Whitespace") { fg = fg.e5 }
+sethl("Whitespace") { fg = fg.v5 }
 
 -- §§1 monaqa-specific
-sethl("VisualBlue") { bg = bg.p4 }
+sethl("VisualBlue") { bg = bg.p2 }
 
 -- §§1 nvim-treesitter
 
@@ -179,55 +207,54 @@ sethl("@boolean") { fg = fg.p4 }
 sethl("@character") {}
 sethl("@character.special") {}
 sethl("@comment") { fg = fg.w5 }
-sethl("@comment.doccomment") { fg = fg.w3, bold = true }
+sethl("@comment.documentation") { fg = fg.w3, bold = true }
 sethl("@conditional") {}
 sethl("@constant") { fg = fg.p2 }
--- sethl("@constant.numeric") { fg = fg.purple4 }
 sethl("@constant.builtin") { fg = fg.p4 }
 sethl("@constant.macro") {}
 sethl("@constructor") {}
 sethl("@debug") {}
 sethl("@define") {}
 sethl("@exception") {}
-sethl("@field") {}
 sethl("@float") {}
 sethl("@function") { fg = fg.e2 }
-sethl("@function.method") { fg = fg.e2 }
+sethl("@function.method") { fg = fg.e1 }
 sethl("@function.builtin") { fg = fg.e3 }
 sethl("@function.call") { fg = fg.e2 }
 sethl("@function.macro") { fg = fg.e3 }
 sethl("@include") {}
 sethl("@keyword") { fg = fg.y2 }
 sethl("@label") {}
+sethl("@markup.raw") { fg = fg.o3 }
+sethl("@markup.heading") { fg = fg.y3, bold = true }
 sethl("@method") {}
 sethl("@method.call") {}
 sethl("@namespace") {}
 sethl("@none") { bg = "NONE", fg = "NONE" }
 sethl("@number") {}
-sethl("@operator") { fg = fg.v3 }
-sethl("@parameter") {}
+sethl("@operator") { fg = fg.v2 }
+sethl("@parameter") { fg = fg.b0 }
 sethl("@preproc") {}
-sethl("@property") {}
+sethl("@property") { fg = fg.w0 }
 sethl("@punctuation") { fg = fg.o0 }
 sethl("@punctuation.delimiter") { fg = fg.o5 }
+sethl("@punctuation.special") { fg = fg.o2 }
 sethl("@repeat") {}
 sethl("@storageclass") {}
 sethl("@string") { fg = fg.g3 }
--- sethl("@string.escape") { fg = fg.g3 }
--- sethl("@string.regex") { fg = fg.g3 }
--- sethl("@string.special") { fg = fg.g4 }
+sethl("@string.documentation") { fg = fg.g1 }
 sethl("@symbol") {}
 sethl("@tag") {}
 sethl("@tag.attribute") {}
 sethl("@tag.delimiter") {}
 sethl("@text") {}
 sethl("@text.danger") {}
-sethl("@text.diff.add") {}
-sethl("@text.diff.addsign") {}
-sethl("@text.diff.change") {}
-sethl("@text.diff.delete") {}
-sethl("@text.diff.delsign") {}
-sethl("@text.diff.indicator") { bg = "#555555" }
+sethl("@text.diff.add") { link = "DiffAdd" }
+sethl("@text.diff.addsign") { fg = fg.g1, bold = true }
+sethl("@text.diff.change") { link = "DiffChange" }
+sethl("@text.diff.delete") { link = "DiffDelete" }
+sethl("@text.diff.delsign") { fg = fg.r1, bold = true }
+sethl("@text.diff.indicator") { bg = bg.w4 }
 sethl("@text.emphasis") { italic = true }
 sethl("@text.environment") {}
 sethl("@text.environment.name") {}
@@ -235,48 +262,64 @@ sethl("@text.literal") { fg = fg.g2 }
 sethl("@text.math") {}
 sethl("@text.note") {}
 sethl("@text.quote") {}
-sethl("@text.reference") {}
+sethl("@text.reference") { fg = fg.b0 }
 sethl("@text.strike") { strikethrough = true }
 sethl("@text.strong") { bold = true }
-sethl("@text.title") { fg = fg.y3, bold = true }
+sethl("@text.title") { fg = fg.y2, bold = true }
 sethl("@text.title.weak") { fg = fg.y0 }
 sethl("@text.todo") {}
 sethl("@text.underline") { underline = true }
-sethl("@text.uri") {}
+sethl("@text.uri") { fg = fg.v5 }
 sethl("@text.warning") {}
-sethl("@type") {}
-sethl("@type.builtin") {}
+sethl("@type") { fg = fg.b2 }
+sethl("@type.builtin") { fg = fg.b3 }
 sethl("@type.definition") {}
 sethl("@type.qualifier") {}
--- sethl("@variable") {}
 sethl("@variable.parameter") { fg = fg.b0 }
--- sethl("@variable.builtin") {}
+sethl("@variable.builtin") { fg = fg.v2 }
 sethl("@variable.member") { fg = fg.b0 }
 
 sethl("@field") { fg = fg.b0 }
 
 -- §§1 plugin-specific
+sethl("GitSignsStagedAddNr") { bg = bg.g4, fg = fg.g0 }
+sethl("GitSignsStagedChangeNr") { bg = bg.b4, fg = fg.b0 }
+sethl("GitSignsStagedDeleteNr") { bg = bg.r4, fg = fg.r0 }
+sethl("GitSignsStagedChangeDeleteNr") { bg = bg.p4, fg = fg.p0 }
+sethl("GitSignsAddNr") { bg = bg.g2, fg = fg.g5 }
+sethl("GitSignsChangeNr") { bg = bg.b2, fg = fg.b5 }
+sethl("GitSignsDeleteNr") { bg = bg.r2, fg = fg.r5 }
+sethl("GitSignsChangeDeleteNr") { bg = bg.p2, fg = fg.p5 }
 
--- §§1 types
----@class HlSpec
----@field fg? string color name or "#RRGGBB", see note.
----@field bg? string color name or "#RRGGBB", see note.
----@field sp? string color name or "#RRGGBB"
----@field blend? integer between 0 and 100
----@field bold? boolean
----@field standout? boolean
----@field underline? boolean
----@field undercurl? boolean
----@field underdouble? boolean
----@field underdotted? boolean
----@field underdashed? boolean
----@field strikethrough? boolean
----@field italic? boolean
----@field reverse? boolean
----@field nocombine? boolean
----@field link? string name of another highlight group to link to, see |:hi-link|.
----@field default? boolean Don't override existing definition |:hi-default|
----@field ctermfg? string Sets foreground of cterm color |ctermfg|
----@field ctermbg? string Sets background of cterm color |ctermbg|
----@field cterm? string cterm attribute map, like |highlight-args|. If not set, cterm attributes will match those from the attribute map documented above.
----@field force? boolean if true force update the highlight group when it exists.
+-- §§1 Vim-syntax aliases
+sethl("Boolean") { link = "@boolean" }
+sethl("Character") { link = "@character" }
+sethl("Conditional") { link = "@conditional" }
+sethl("Constant") { link = "@constant" }
+sethl("Debug") { link = "@debug" }
+sethl("Define") { link = "@define" }
+sethl("Delimiter") { link = "@punctuation.delimiter" }
+sethl("Exception") { link = "@exception" }
+sethl("Float") { link = "@float" }
+sethl("Function") { link = "@function" }
+sethl("Identifier") { link = "@symbol" }
+sethl("Include") { link = "@include" }
+sethl("Keyword") { link = "@keyword" }
+sethl("Label") { link = "@label" }
+sethl("Macro") { link = "@function.macro" }
+sethl("Number") { link = "@number" }
+sethl("Operator") { link = "@operator" }
+sethl("PreProc") { link = "@preproc" }
+sethl("Quote") { link = "@text.quote" }
+sethl("Repeat") { link = "@repeat" }
+sethl("Special") { link = "@constant.builtin" }
+sethl("SpecialChar") { link = "@character.special" }
+sethl("SpecialComment") { link = "@text.note" }
+sethl("StorageClass") { link = "@storageclass" }
+sethl("String") { link = "@string" }
+sethl("Tag") { link = "@tag" }
+sethl("Title") { link = "@text.title" }
+sethl("Todo") { link = "@text.todo" }
+sethl("Type") { link = "@type" }
+sethl("Typedef") { link = "@type.definition" }
+sethl("Underlined") { link = "@text.uri" }
