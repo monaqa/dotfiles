2f9797a41903cad4b7d35f9c2bdef70e97e2a33e
monaqa <cmonaqa@gmail.com>
2024-07-23 18:28:01 +0900

    update bufferline layout

--- .config/nvim/queries/diff/highlights.scm
+++
@@ -1,9 +0,0 @@
-(addition ("+") @text.diff.addsign) @text.diff.add
-(deletion ("-") @text.diff.delsign) @text.diff.delete
-
-(new_file) @text.diff.addsign
-(old_file) @text.diff.delsign
-
-(commit) @constant
-(location) @attribute
-(command) @function
